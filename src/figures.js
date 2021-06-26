6d153983887aaecdb6d20bc1447ecdc8d3550a30
nelwey <nelwey1234@gmail.com>
2021-06-26 01:07:53 -0400

    adding functions

--- src/figures.js
+++ src/figures.js
@@ -1,40 +1,29 @@
 // Square
-const sideSquare = 5;
-const perimeterSquare = sideSquare * 4;
-const areaSquare = sideSquare ** 2;
-
-console.group("Square");
-console.log(`The square side are ${sideSquare}cm`);
-console.log(`The square perimeter is ${perimeterSquare}cm`);
-console.log(`The square area is ${areaSquare}cm^2`);
-console.groupEnd();
-
+const perimeterSquare = (sideSquare) => {
+  return sideSquare * 4;
+}
+const areaSquare = (sideSquare) => {
+  return sideSquare ** 2;
+}
 // Triangle
-const triangleSides = [6, 6, 4];
-const triangleBase = triangleSides[2];
-const triangleHeight = 5.5;
-const trianglePerimeter =
-  triangleSides[0] + triangleSides[1] + triangleSides[2];
-const triangleArea = (triangleBase * triangleHeight) / 2;
 
-console.group("Triangle");
-console.log(`The triangle sides are ${triangleSides}cm`);
-console.log(`The triangle height is ${triangleHeight}cm`);
-console.log(`The triangle perimeter is ${trianglePerimeter}cm`);
-console.log(`The triangle area is ${triangleArea}cm^2`);
-console.groupEnd();
+const trianglePerimeter = (sideA, sideB, base) => {
+  return sideA + sideB + base;
+}
+
+const triangleArea = (base, height) => {
+  return (base * height) / 2;
+}
 
 // Circle
-const circleRadius = 4;
-const circleDiameter = circleRadius + circleRadius;
-const PI = Math.PI;
-const circlePerimeter = circleDiameter * PI;
-const circleArea = circleRadius * circleRadius * PI;
+const circleDiameter = (radius) => {
+  return radius * 2;
+}
+const circlePerimeter = (radius) => {
+  const diameter = circleDiameter(radius);
+  return diameter * Math.PI;
+}
 
-console.group("Circle");
-console.log(`The circle radius is ${circleRadius}`);
-console.log(`The circle diameter is ${circleDiameter}`);
-console.log(`PI is ${PI}`);
-console.log(`The circle perimeter is ${circlePerimeter}`);
-console.log(`The circle area is ${circleArea}`);
-console.groupEnd();
+const circleArea = (radius) => {
+  return radius * radius * Math.PI;
+}
