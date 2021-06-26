8e3b9e4ce69e7cf0868f4aabe1c7866c4498bfe7
nelwey <nelwey1234@gmail.com>
2021-06-26 02:07:55 -0400

    adding html form

--- src/figures.js
+++ src/figures.js
@@ -8,7 +8,7 @@ const areaSquare = (sideSquare) => {
 // Triangle
 
 const trianglePerimeter = (sideA, sideB, base) => {
-  return sideA + sideB + base;
+  return Number(sideA) + Number(sideB) + Number(base);
 }
 
 const triangleArea = (base, height) => {
@@ -26,4 +26,47 @@ const circlePerimeter = (radius) => {
 
 const circleArea = (radius) => {
   return radius * radius * Math.PI;
+}
+
+
+const squarePerimeterCalc = () => {
+  const input = document.getElementById("squareInput").value;
+  const res = perimeterSquare(input);
+  alert(res);
+}
+
+const squareAreaCalc = () => {
+  const input = document.getElementById("squareInput").value;
+  const res = areaSquare(input);
+  alert(res);
+}
+
+const trianglePerimeterCalc = () => {
+  const sideA = document.getElementById("sideATriangle").value;
+  const sideB = document.getElementById("sideBTriangle").value;
+  const base = document.getElementById("baseTriangle").value;
+  const res = trianglePerimeter(sideA, sideB, base);
+  alert(res);
+}
+const triangleAreaCalc = () => {
+  const base = document.getElementById("baseTriangle").value;
+  const height = document.getElementById("heightTriangle").value;
+  const res = triangleArea(base, height);
+  alert(res);
+}
+
+const circlePerimeterCalc = () => {
+  const input = document.getElementById("circleRadius").value;
+  const res = circlePerimeter(input);
+  alert(res);
+}
+const circleAreaCalc = () => {
+  const input = document.getElementById("circleRadius").value;
+  const res = circleArea(input);
+  alert(res);
+}
+const circleDiameterCalc = () => {
+  const input = document.getElementById("circleRadius").value;
+  const res = circleDiameter(input);
+  alert(res);
 }
